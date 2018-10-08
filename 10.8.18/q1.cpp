591ebc99af7ff187f4aebfb4fefd6db6f4322396
flastest <pleasantbowel5yndrome@gmail.com>
2018-10-07 19:20:46 -0700

    finished question one, comparison of 3 numbers

--- 10.8.18/q1.cpp
+++ 10.8.18/q1.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 
-int main(){
+int main() {
 	int firstNumber;
 	int secondNumber;
 	int thirdNumber;
-	cout << "Enter three integers";
-	cin >> firstNumber >> secondNumber >> thirdNumber;
+	std::cout << "Enter three integers\n";
+	std::cin >> firstNumber >> secondNumber >> thirdNumber;
 
-	int biggestNum;
-	if(){
-		
+	int biggestNum= firstNumber;
+
+	if(biggestNum < secondNumber) {
+		biggestNum = secondNumber;
 	}
+	if (biggestNum < thirdNumber) {
+		biggestNum = thirdNumber;
+	}
+
+	std::cout << "The biggest number is ";
+	std::cout << biggestNum;
+	return 0;
 }
