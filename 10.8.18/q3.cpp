fb71886d618216fe9eed3a2ec135da322b2ce4d9
flastest <pleasantbowel5yndrome@gmail.com>
2018-10-07 22:09:22 -0700

    added comments!

--- 10.8.18/q3.cpp
+++ 10.8.18/q3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-
+// Calculates base multiplied by itself timesToMultiply
+// amount of times. Returns base to the power of
+// timesToMultiply
 int pow(int base, int timesToMultiply) {
 	if(timesToMultiply <= 1){
 		return base;
@@ -9,6 +11,14 @@ int pow(int base, int timesToMultiply) {
 	return base*pow(base, timesToMultiply);
 }
 
+//from Eitan:
+// The main function should read in two integer values from the user. 
+// If either input is less than 1, the main function should print out 
+// "Both inputs must be positive" and return. If both inputs are 
+// positive, the main function should call pow, specifying the first 
+// user input as the first argument (i.e. base) and the second user 
+// input as the second argument (i.e. exponent). The main function 
+// should then print the result returned by pow before returning.
 int main() {
 	int base;
 	int exponent;					// intializing the variables
