70e9586d2fb1dcd7ea1111213e33903d1d5f51f5
flastest <pleasantbowel5yndrome@gmail.com>
2018-10-07 17:18:34 -0700

    started working. commiting to start good habits

---
+++ 10.8.18/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+	int firstNumber;
+	int secondNumber;
+	int thirdNumber;
+	cout << "Enter three integers";
+	cin >> firstNumber >> secondNumber >> thirdNumber;
+
+	int biggestNum;
+	if(){
+		
+	}
+}
