05fdfafdb9b5fcba3dc20e6b5d9fd3b72ab2266f
flastest <pleasantbowel5yndrome@gmail.com>
2018-10-25 19:03:38 -0700

    sorry, I had to change key_t to my_key_t because I'm using cygwin. forgive me

--- 10.29.18 (trees)/test_tree.cc
+++ 10.29.18 (trees)/test_tree.cc
@@ -1,4 +1,90 @@
 //This will test the tree class
-main()	{
+////
+#include "tree.hh"
+#include <stdint.h>
+#include <iostream>
+
+using std::cout;
+//using key_t = int;
+//using value_t = uint64_t;
+//using tree_ptr_t = Tree*;
+
+static void test1(){	
+
+	//gonna make this guy:
+	/*
+            126
+           /   \
+          /     \
+        -5       12
+        / \     /
+      12   6    3
+          /
+         9
+	*/
+
+	//tree 9
+	my_key_t num = 9;
+	value_t val = 9;
+	tree_ptr_t tree9 = create_tree(num,val);
+
+	//tree 6
+	num = 6;
+	val = 6;
+	tree_ptr_t left = tree9;
+	tree_ptr_t tree6 = create_tree(num,val, left);
+
+	//tree 12L-most
+	num =  12;
+	val = 12; 
+	tree_ptr_t tree12L = create_tree(num,val);
+
+	//tree -5
+	num =  -5;
+	val = 5; 
+	left = tree12L;
+	tree_ptr_t right = tree6;
+	tree_ptr_t treeNeg5 = create_tree(num,val,left,right);
+
+	//tree 3
+	num =  3;
+	val = 3; 
+	tree_ptr_t tree3 = create_tree(num,val); 
+
+	//tree 12R
+	num =  12;
+	val = 12; 
+	left = tree3;
+	tree_ptr_t tree12R = create_tree(num,val,left); 
+
+	//tree 126 (head)
+	num =  126;
+	val = 126; 
+	left = treeNeg5;
+	right = tree12R;
+	tree_ptr_t tree126 = create_tree(num,val,left,right); 
 	
+	int search = 9;
+	cout<<"path to " << search << " is " +path_to(tree126,search) + "\n";
+	
+	cout <<"and the thing at that node is " <<  node_at(tree126,path_to(tree126,search))->key_ << " \n";
+
+	destroy_tree(tree9);
+	/*for (int i = 0; i < 5; i++){
+		my_key_t key = i;
+		value_t value = 0;
+		if(i%2==0){
+			Tree* left = i+1;
+			Tree* right = nullptr;
+		} else {
+			Tree* left = nullptr;
+			Tree* right = i+1;
+		}
+		Tree* = create_tree(key, value, left, right);	
+	}*/
+}
+
+int main()	{
+	test1();
+
 }
--- 10.29.18 (trees)/tree.cc
+++ 10.29.18 (trees)/tree.cc
@@ -1,53 +1,95 @@
 #include <string>
+#include "tree.hh"
+#include <iostream>
+#include <assert.h>
 
-using tree_ptr_t = Tree*;
+using std::cout; 
+using std::string;
                                 // why use & instead of asterisk?
-tree_ptr_t create_tree(const key_t& key, const value_t& value, tree_ptr_t left = nullptr,
-                       tree_ptr_t right = nullptr) {
-	auto tmp = new Tree;
+tree_ptr_t create_tree(const my_key_t& key, const value_t& value, tree_ptr_t left, tree_ptr_t right) {
+	Tree* tmp = new Tree;
 	tmp -> key_ = key;
 	tmp -> value_ = value;
 	tmp -> left_ = left;
-	temp -> right_ = right;
-	return tmp*;
+	tmp -> right_ = right;
+	return tmp;
 }
 
 
 void destroy_tree(tree_ptr_t tree) {
-	if (tree = nullptr) {
+	if (tree == nullptr) {
 		return;
 	}
-	destroy_tree(*tree -> right_);
-	destroy_tree(*tree -> left_);
-	delete *tree;
+	destroy_tree(tree -> right_);
+	destroy_tree(tree -> left_);
+	delete tree;
+	cout << "tree been desroyed\n";
 }
 
-bool isValueHere(tree_ptr_t tree, key_t key){
+bool isValueHere(tree_ptr_t tree, my_key_t key){
 	if (tree == nullptr) {
 		return false;
 	}
-	if (tree* -> key_ == key) {
+	if (tree -> key_ == key) {
 		return true;
 	}
-	return (isValueHere(tree*->left_) || isValueHere(tree*->right_));
+	return (isValueHere(tree->left_,key) || isValueHere(tree->right_,key));
 
 }
 
-std::string path_to(tree_ptr_t tree, key_t key) {
+std::string path_to(tree_ptr_t tree, my_key_t key) {
 	std::string path = "";
-	if (tree* -> key_ = key) {
+	if (tree -> key_ == key) {
 		return path;
 	}
-	if (isValueHere(tree* -> left_, key)) {
-		path = path + "L" + path_to(tree* -> left_, key);
+	if (isValueHere(tree -> left_, key)) {
+		path = path + "L" + path_to(tree -> left_, key);
 	}
-	else if (isValueHere(tree* -> right_, key)) {
-		path = path + "R" + path_to(tree* -> left_, key);	
+	else if (isValueHere(tree -> right_, key)) {
+		path = path + "R" + path_to(tree -> right_, key);	
 	}
 	else {
-		assert false;
+		assert(false);
 	}
 	return path;
 }
 
 
+
+char subString(string input) {
+	if (input==""){
+		return ' ';
+	}
+	char R = 'R';
+	char L = 'L';
+	return (input.at(0)==R)? R: (input.at(0)==L)? L: ' ';
+}
+
+string suffix(string input) {
+	if(input.size()>1) {
+		return (input.substr(1));
+	}
+	return "";
+}
+
+/*bool checkIfValid(string input){
+	if (!input){
+		return false;
+	}
+	if (input != "R")
+}*/
+
+tree_ptr_t node_at(tree_ptr_t tree, string path) {
+	if (path==""||tree==nullptr){
+		return tree;
+	}
+	char curChar = subString(path);
+	string restOfStuff = suffix(path); 
+	if (curChar == 'R') {
+		return node_at(tree->right_, restOfStuff);	
+	}
+	if (curChar == 'L') {
+		return node_at(tree->left_, restOfStuff);	
+	}
+	return nullptr;
+}
