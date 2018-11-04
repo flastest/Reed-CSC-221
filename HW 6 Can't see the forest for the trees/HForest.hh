d61bbbebb5d0f8dfeb1fa3beda7713c8c1f413be
flastest <pleasantbowel5yndrome@gmail.com>
2018-11-03 17:16:44 -0700

    I pray that this does not commit the exe files too! I didn't add them!

--- HW 6 Can't see the forest for the trees/HForest.hh
+++ HW 6 Can't see the forest for the trees/HForest.hh
@@ -4,15 +4,16 @@
 #include <algorithm>
 #include <queue>          // std::priority_queue
 
+using tree_ptr_t = std::shared_ptr<const HTree>;
 
 class HForest { 
 public:
 	HForest(){}
 	int size() const;
-	void addTree(HTree* tree);
-	HTree* popTree();
+	void addTree(tree_ptr_t tree);
+	tree_ptr_t popTree();
 private:
 
 	//std::list<HTree*> listOfTrees;
-	std::priority_queue<HTree*> listOfTrees;
+	std::priority_queue<tree_ptr_t> listOfTrees;
 };
