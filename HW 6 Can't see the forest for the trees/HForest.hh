9b73c61126b6a1fa2c045e538c1b175d0299393a
flastest <pleasantbowel5yndrome@gmail.com>
2018-11-03 18:13:43 -0700

    did some freaky stuff

--- HW 6 Can't see the forest for the trees/HForest.hh
+++ HW 6 Can't see the forest for the trees/HForest.hh
@@ -6,14 +6,34 @@
 
 using tree_ptr_t = std::shared_ptr<const HTree>;
 
+
+class compareTrees
+    {
+        public:
+            bool operator() (const tree_ptr_t &t1, const tree_ptr_t &t2) const
+            {
+                return t1->get_key() < t2->get_key();
+            }
+    };
+    
+
+
 class HForest { 
 public:
-	HForest(){}
+	//constructor
+	HForest();
+
+	//returns the size of the forest
 	int size() const;
+
+	//adds a tree to forest
 	void addTree(tree_ptr_t tree);
+	
+	//returns a pointer to a tree and removes it from the forest.
 	tree_ptr_t popTree();
 private:
 
-	//std::list<HTree*> listOfTrees;
-	std::priority_queue<tree_ptr_t> listOfTrees;
+	//made a priority queue because that's easier
+	std::priority_queue<tree_ptr_t, std::vector<tree_ptr_t>, compareTrees> listOfTrees;
 };
+
--- HW 6 Can't see the forest for the trees/forestTest.cc
+++ HW 6 Can't see the forest for the trees/forestTest.cc
@@ -4,6 +4,7 @@
 using tree_ptr_t = std::shared_ptr<const HTree>;
 using std::cout;
 
+//recursively creates arbitrary trees from a list
 tree_ptr_t makeTree(std::vector<int> listT){
 	if(listT.size()<1){
 		return nullptr;
@@ -31,15 +32,21 @@ int main (){
 		966,2,3,4,5,8});
 	tree_ptr_t testTree2 = makeTree(std::vector<int>{
 		967,556,6,7,6});
+	tree_ptr_t testTree3 = makeTree(std::vector<int>{
+		963,723,623});
 
 	HForest billyJoel;
+	//verify addTree works
 	billyJoel.addTree(testTree1);
 	billyJoel.addTree(testTree2);
+	billyJoel.addTree(testTree3);
 
 	cout<<"tree 1 is " << (*testTree1).GetDebugString()<<"\n";
 	cout<<"tree 2 is " << (*testTree2).GetDebugString()<<"\n";
 
+	//verify that popTree works
 	cout<<"pop "<<(billyJoel.popTree())->get_key()<<"\n";
+	cout<<"pop again "<<(billyJoel.popTree())->get_key()<<"\n";
 	cout<<"pop again "<<(billyJoel.popTree())->get_key()<<"\n";
 	cout<<"diddlydone\n";
 
