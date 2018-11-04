3aac78203019e37326a95af47d1c7bb9e379360b
flastest <pleasantbowel5yndrome@gmail.com>
2018-11-03 18:11:29 -0700

    did priority queue

--- HW 6 Can't see the forest for the trees/hforest.cc
+++ HW 6 Can't see the forest for the trees/hforest.cc
@@ -4,26 +4,25 @@
 
 using tree_ptr_t = std::shared_ptr<const HTree>;
 
+
+
 int HForest::size() const {
 	return HForest::listOfTrees.size();
 }
 
-//is HTree* a pointer or a thingy?
-bool compareTrees(tree_ptr_t t1, tree_ptr_t  t2) {
-    return t1->get_value() < t2->get_value();
+
+HForest::HForest() 
+	: listOfTrees(compareTrees())
+{
 }
 
 void HForest::addTree(tree_ptr_t tree) {
-	//HForest::listOfTrees.push_back(tree);
-	//std::push_heap<std::list<HTree*>::iterator>(listOfTrees.begin(),listOfTrees.end()
-	//,compareTrees);
+	//adds a tree to the priority queue
 	listOfTrees.push(tree);
 }
 
 tree_ptr_t HForest::popTree() {
-	//HTree* thing = (*listOfTrees.begin());
-	//HForest::listOfTrees.pop_back();
-	//std::pop_heap(HForest::listOfTrees.begin(),HForest::listOfTrees.end(),compareTrees);	
+	//pops a tree from the priority queue
 	tree_ptr_t thing = listOfTrees.top();
 	listOfTrees.pop();
 	return thing;
