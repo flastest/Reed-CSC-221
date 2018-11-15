d2eb7824e264b79446ec4f08235a465b60969840
flastest <pleasantbowel5yndrome@gmail.com>
2018-11-15 09:47:44 -0800

    added << and >> for cities

--- HW 8 TSP/cities.cc
+++ HW 8 TSP/cities.cc
@@ -1,20 +1,39 @@
 #include "cities.hh"
 #include <cmath>
+#include <random>
+#include <algorithm>
+#include <iterator>
 
 std::istream & operator>>(std::istream & is, Cities& city){
-    Cities::permutation_t city_order = city.get_permutation();
-    for (int i : city_order){
-        is >> city_order[i].first >> city_order[i].second;
+    int x;
+    int y;
+    std::vector<Cities::coord_t> list;
+    while (is>>x){
+        Cities::coord_t pair;
+        pair.first = x;
+        is >> y;
+        pair.second = y;
+        list.push_back(pair);
     }
+    city.cities_ = list;
     return is;
 }
 
 std::ostream & operator<<(std::ostream & os, Cities& city){
-    Cities::permutation_t city_order = city.get_permutation();
-    for (int i : city_order){
-        os << city_order[i].first << " " << city_order[i].second << "\n";
+    int x;
+    int y;
+    std::vector<Cities::coord_t> list;
+    list = city.cities_;
+    Cities::coord_t pair;
+    for (int i = 0; i < list.size(); i ++){
+        pair = list[i];
+        x = pair.first;
+        y = pair.second;
+        os<<x;
+        os<<y;
     }
     return os;
+
 }
 
 double Cities::path_between(coord_t t1, coord_t t2) const{
@@ -33,5 +52,16 @@ double Cities::total_path_distance(const permutation_t& ordering) const{
 }
 
 Cities::Cities Cities::reorder(const permutation_t& ordering) const{
-    
+    return nullptr;
+}
+
+permutation_t Cities::random_permutation(unsigned len){
+    std::std::vector<int> v(len);
+    for (int i = 0; i < len; i ++) {
+        vector[i] = i;
+    }
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(v.begin(),v.end(),g);
+    return reorder(&v);
 }
--- HW 8 TSP/cities.hh
+++ HW 8 TSP/cities.hh
@@ -10,12 +10,17 @@
 // Representation of an ordering of cities
 class Cities {
  public:
+
   // A pair of integral coordinates for each city
   using coord_t = std::pair<int, int>;
 
   // An ordering of the cities in cities_t. Each value represents a unique index
   // into the current city ordering.
   using permutation_t = std::vector<unsigned int>;
+
+
+  //we need a public vector of cities that doesn't ever get permuted
+  std::vector<coord_t> cities_;
 
   // Given a permutation, return a new Cities object where the order of the
   // cities reflects the original order of this class after reordering with
@@ -30,12 +35,12 @@ class Cities {
   // distance on a plane between their coordinates.
   double total_path_distance(const permutation_t& ordering) const;
 
-  //Implement operator>> (std::istream&, Cities&) to allow for the reading  / filling of a Cities object from a stream. 
-  std::istream& operator>>(std::istream& is, Cities& city);
 
-  std::ostream& operator<<(std::ostream& is, Cities& city);
 
   permutation_t get_permutation(){return order_of_cities;}
+
+  //this returns a random permutation of cities
+  permutation_t random_permutation(unsigned len);
 
 
 private:
@@ -45,3 +50,9 @@ private:
 
   
 };
+
+
+//Implement operator>> (std::istream&, Cities&) to allow for the reading  / filling of a Cities object from a stream. 
+std::istream& operator>>(std::istream& is, Cities& city);
+
+std::ostream& operator<<(std::ostream& is, Cities& city);
